c74582bdcfd696196e720618e28f608fb64b0b05
Jennifer Castro <jcastro@zenoss.com>
2016-05-22 18:45:39 -0600

    Fist commit of project, it has the code for the task of session 1

---
+++ atTrainingJavaFramework/src/main/java/com/gap/at/Test.java
@@ -0,0 +1,30 @@
+package com.gap.at;
+
+import com.gap.at.selenium.SeleniumBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by jcastro on 5/22/16.
+ */
+public class Test {
+
+    public static void main(String [] args){
+
+        SeleniumBase seleniumBase = new SeleniumBase();
+        WebDriver driver = seleniumBase.initDriver("HtmlUnit");
+
+        driver.get("http://www.google.com/xhtml");
+        try {
+            Thread.sleep(5000);  // Let the user actually see something!
+            WebElement we = driver.findElement(By.name("q"));
+            we.sendKeys("Selenium");
+            we.submit();
+            Thread.sleep(5000);  // Let the user actually see something!
+            driver.quit();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ atTrainingJavaFramework/src/main/java/com/gap/at/selenium/SeleniumBase.java
@@ -0,0 +1,59 @@
+package com.gap.at.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+/**
+ * Created by jcastro on 5/22/16.
+ */
+public class SeleniumBase {
+
+    private WebDriver driver;
+
+    /**
+     * This method create a instance of the driver
+     *
+     * @param browserName Browser name
+     * @return Driver to use
+     */
+    public WebDriver initDriver(String browserName){
+
+        switch (browserName) {
+            case "Chrome":
+                this.driver = initChromeDriver();
+                break;
+            case "IE":
+                this.driver = initIEDriver();
+                break;
+            case "Firefox":
+                this.driver = initFirefoxDriver();
+                break;
+            case "HtmlUnit":
+                initHTMLUnitDriver();
+                break;
+            default:
+                this.driver = null;
+                break;
+        }
+        return this.driver;
+    }
+
+    private WebDriver initChromeDriver(){
+        return new ChromeDriver();
+    }
+
+    private WebDriver initIEDriver(){
+        return new InternetExplorerDriver();
+    }
+
+    private WebDriver initFirefoxDriver(){
+        return new FirefoxDriver();
+    }
+
+    private WebDriver initHTMLUnitDriver(){
+        return new HtmlUnitDriver();
+    }
+}
