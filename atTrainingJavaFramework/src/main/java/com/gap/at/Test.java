5e7119da26b9c629b58482b7c40d373706aa2406
Jennifer Castro <jcastro@zenoss.com>
2016-07-03 20:22:29 -0600

    Minor changes

--- atTrainingJavaFramework/src/main/java/com/gap/at/Test.java
+++ atTrainingJavaFramework/src/main/java/com/gap/at/Test.java
@@ -1,5 +1,6 @@
 package com.gap.at;
 
+import com.gap.at.utils.TakeScreenshot;
 import com.gap.at.selenium.SeleniumBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,15 +14,17 @@ public class Test {
     public static void main(String [] args){
 
         SeleniumBase seleniumBase = new SeleniumBase();
-        WebDriver driver = seleniumBase.initDriver("HtmlUnit");
+        WebDriver driver = seleniumBase.initDriver("Chrome");
 
         driver.get("http://www.google.com/xhtml");
+        TakeScreenshot.takeScreenshot(driver, "./src/main/resources/screenshots/screenshot1");
         try {
             Thread.sleep(5000);  // Let the user actually see something!
             WebElement we = driver.findElement(By.name("q"));
             we.sendKeys("Selenium");
             we.submit();
             Thread.sleep(5000);  // Let the user actually see something!
+            TakeScreenshot.takeScreenshot(driver, "./src/main/resources/screenshots/screenshot2");
             driver.quit();
         } catch (InterruptedException e) {
             e.printStackTrace();
