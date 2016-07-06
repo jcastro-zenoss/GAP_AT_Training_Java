959be2b963279f3c29ec8cb012fe2a6f9753c66d
Jennifer Castro <jcastro@zenoss.com>
2016-07-06 16:27:04 -0600

    Code completed for session 1

--- src/main/java/com/gap/at/Test.java
+++ src/main/java/com/gap/at/Test.java
@@ -1,6 +1,5 @@
 package com.gap.at;
 
-import com.gap.at.utils.TakeScreenshot;
 import com.gap.at.selenium.SeleniumBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -17,14 +16,14 @@ public class Test {
         WebDriver driver = seleniumBase.initDriver("Chrome");
 
         driver.get("http://www.google.com/xhtml");
-        TakeScreenshot.takeScreenshot(driver, "./src/main/resources/screenshots/screenshot1");
+
         try {
             Thread.sleep(5000);  // Let the user actually see something!
             WebElement we = driver.findElement(By.name("q"));
             we.sendKeys("Selenium");
             we.submit();
             Thread.sleep(5000);  // Let the user actually see something!
-            TakeScreenshot.takeScreenshot(driver, "./src/main/resources/screenshots/screenshot2");
+
             driver.quit();
         } catch (InterruptedException e) {
             e.printStackTrace();
