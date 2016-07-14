e81d6867643116214d6b658ee6b4c6106601efb0
Jennifer Castro <jcastro@zenoss.com>
2016-07-13 20:06:25 -0600

    Add Testlink dependency to pom and Testlink class in progress

---
+++ src/main/java/com/gap/at/testmanagementsystem/testlink/TestLinkAccess.java
@@ -0,0 +1,30 @@
+package com.gap.at.testmanagementsystem.testlink;
+
+
+import br.eti.kinoshita.testlinkjavaapi.TestLinkAPI;
+import br.eti.kinoshita.testlinkjavaapi.constants.ExecutionStatus;
+import br.eti.kinoshita.testlinkjavaapi.constants.ExecutionType;
+import br.eti.kinoshita.testlinkjavaapi.constants.TestCaseDetails;
+import br.eti.kinoshita.testlinkjavaapi.model.Build;
+import br.eti.kinoshita.testlinkjavaapi.model.ReportTCResultResponse;
+import br.eti.kinoshita.testlinkjavaapi.model.TestCase;
+import br.eti.kinoshita.testlinkjavaapi.util.TestLinkAPIException;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by jcastro on 7/13/16.
+ */
+public class TestLinkAccess extends TestLinkAPI{
+
+    public TestLinkAccess(String URL, String devKey) throws MalformedURLException {
+        super(new URL(URL), devKey);
+    }
+
+    public ReportTCResultResponse setResult(int testCaseId, int testPlanId, int buildID, String notes, String status) throws TestLinkAPIException, MalformedURLException
+    {
+        return this.setTestCaseExecutionResult(testCaseId, null, testPlanId, ExecutionStatus.getExecutionStatus(status.charAt(0)), buildID, null, notes, true, null, null, null, null, false);
+    }
+
+}
