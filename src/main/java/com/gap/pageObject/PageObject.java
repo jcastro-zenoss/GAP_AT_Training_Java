24b708be25c5dbd25572bc8a162535713c7712e5
jpolanco <jpolanco@growthaccelerationpartners.com>
2016-07-27 15:36:36 -0600

    Adding PO and Utils

---
+++ src/main/java/com/gap/pageObject/PageObject.java
@@ -0,0 +1,7 @@
+package com.gap.pageObject;
+
+/**
+ * Created by polanquillo on 7/27/16.
+ */
+public class PageObject {
+}
---
+++ src/main/java/com/gap/utils/Utils.java
@@ -0,0 +1,7 @@
+package com.gap.utils;
+
+/**
+ * Created by polanquillo on 7/27/16.
+ */
+public class Utils {
+}
