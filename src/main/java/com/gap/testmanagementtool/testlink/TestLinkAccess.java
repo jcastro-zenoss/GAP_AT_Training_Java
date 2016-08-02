ee69385f6ae5d259ab5ba8d65a0965370d5720bc
Jennifer Castro <jcastro@zenoss.com>
2016-08-02 16:39:52 -0600

    Adding a method to add a test plan in testlink

--- src/main/java/com/gap/testmanagementtool/testlink/TestLinkAccess.java
+++ src/main/java/com/gap/testmanagementtool/testlink/TestLinkAccess.java
@@ -4,6 +4,7 @@ package com.gap.testmanagementtool.testlink;
 import br.eti.kinoshita.testlinkjavaapi.TestLinkAPI;
 import br.eti.kinoshita.testlinkjavaapi.constants.ExecutionStatus;
 import br.eti.kinoshita.testlinkjavaapi.model.ReportTCResultResponse;
+import br.eti.kinoshita.testlinkjavaapi.model.TestProject;
 import br.eti.kinoshita.testlinkjavaapi.util.TestLinkAPIException;
 
 import java.net.MalformedURLException;
@@ -14,13 +15,35 @@ import java.net.URL;
  */
 public class TestLinkAccess extends TestLinkAPI{
 
-    public TestLinkAccess(String URL, String devKey) throws MalformedURLException {
+    public TestLinkAccess(String URL, String devKey) throws TestLinkAPIException, MalformedURLException {
         super(new URL(URL), devKey);
     }
 
+    /**
+     * This method create a new Testlink project
+     * @param testProjectName Project name
+     * @param testProjectPrefix Prefix (used for Test case ID)
+     * @param notes Description of the project
+     * @param enableRequirements Boolean to enable requirements
+     * @param enableTestPriority Boolean to enable test priority
+     * @param enableAutomation Boolean to enable Automation
+     * @param enableInventory Boolean to enable inventory
+     * @param isActive Active project
+     * @param isPublic Public project
+     * @return a TestProject object
+     */
+    public TestProject createTestlinkProject(String testProjectName, String testProjectPrefix, String notes, Boolean enableRequirements,
+                                         Boolean enableTestPriority, Boolean enableAutomation, Boolean enableInventory, Boolean isActive,
+                                         Boolean isPublic){
+
+        return this.createTestProject(testProjectName, testProjectPrefix, notes, enableRequirements, enableTestPriority,
+                                      enableAutomation, enableInventory, isActive, isPublic);
+    }
+
+
     public ReportTCResultResponse setResult(int testCaseId, int testPlanId, int buildID, String notes, String status) throws TestLinkAPIException, MalformedURLException
     {
-        return this.setTestCaseExecutionResult(testCaseId, null, testPlanId, ExecutionStatus.getExecutionStatus(status.charAt(0)), buildID, null, notes, true, null, null, null, null, false);
+        return setTestCaseExecutionResult(testCaseId, null, testPlanId, ExecutionStatus.getExecutionStatus(status.charAt(0)), buildID, null, notes, true, null, null, null, null, false);
     }
 
 }
