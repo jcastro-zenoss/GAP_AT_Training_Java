5c1d8008b17469b4d562dcd5fa125e1a3eac1f2b
Jennifer Castro <jcastro@zenoss.com>
2016-08-02 16:39:52 -0600

    Adding a method to add a test plan in testlink

--- src/main/java/com/gap/testSuites/Test.java
+++ src/main/java/com/gap/testSuites/Test.java
@@ -1,9 +1,12 @@
 package com.gap.testSuites;
 
 import com.gap.selenium.SeleniumBase;
+import com.gap.testmanagementtool.testlink.TestLinkAccess;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
+import java.net.MalformedURLException;
 
 /**
  * Created by jcastro on 5/22/16.
@@ -12,6 +15,24 @@ public class Test {
 
     public static void main(String [] args){
 
+        String url = "https://testlink.wearegap.com/lib/api/xmlrpc/v1/xmlrpc.php";
+        String key = "80535d62d292f57003486de62441abe8";
+
+        try {
+            createTestLinkProject(url, key);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createTestLinkProject(String url, String key) throws MalformedURLException {
+
+        TestLinkAccess testLinkAccess = new TestLinkAccess(url, key);
+        testLinkAccess.createTestlinkProject("TestAutomation1", "AT-", "This is a Testlink project", false, true, true, false, true, true);
+
+    }
+
+    public static void test1(){
         SeleniumBase seleniumBase = new SeleniumBase();
         WebDriver driver = seleniumBase.initDriver("Chrome");
 
